1658c92b8272b750ca3c0c36c26e05890510de47
Nebual <ben1120@gmail.com>
2013-08-02 12:31:51 -0700

    Thrusters, Textscreen, FXEmitters: Edits for single file ent Renames in previous commit This commit was purely merging files, all edits were 2 commits ago

--- lua/entities/gmod_wire_vectorthruster.lua
+++ lua/entities/gmod_wire_vectorthruster.lua
@@ -1,10 +1,131 @@
+AddCSLuaFile()
+DEFINE_BASECLASS( "base_wire_entity" )
+ENT.PrintName       = "Wire Vector Thruster"
+// Thrusters only really need to be twopass (Both) when they're active.. something to think about..
+ENT.RenderGroup 		= RENDERGROUP_BOTH
+ENT.WireDebugName	= "Vector Thruster"
 
-AddCSLuaFile( "cl_init.lua" )
-AddCSLuaFile( "shared.lua" )
 
-include('shared.lua')
+-- Shared
 
-ENT.WireDebugName = "Vector Thruster"
+function ENT:SetEffect( name )
+	self:SetNetworkedString( "Effect", name )
+end
+function ENT:GetEffect()
+	return self:GetNetworkedString( "Effect" )
+end
+
+function ENT:SetOn( boolon )
+	self:SetNetworkedBool( "vecon", boolon, true )
+end
+function ENT:IsOn()
+	return self:GetNetworkedBool( "vecon" )
+end
+
+function ENT:SetMode( v )
+	self:SetNetworkedInt( "vecmode", v, true )
+end
+function ENT:GetMode()
+	return self:GetNetworkedInt( "vecmode" )
+end
+
+function ENT:SetOffset( v )
+	self:SetNetworkedVector( "Offset", v, true )
+end
+function ENT:GetOffset( name )
+	return self:GetNetworkedVector( "Offset" )
+end
+
+function ENT:SetNormal( v )
+	self:SetNetworkedInt( "vecx", v.x * 100, true )
+	self:SetNetworkedInt( "vecy", v.y * 100, true )
+	self:SetNetworkedInt( "vecz", v.z * 100, true )
+end
+function ENT:GetNormal()
+	return Vector(
+				self:GetNetworkedInt( "vecx" ) / 100,
+				self:GetNetworkedInt( "vecy" ) / 100,
+				self:GetNetworkedInt( "vecz" ) / 100
+			)
+end
+
+function ENT:NetSetForce( force )
+	self:SetNetworkedInt("vecforce", math.floor(force*100))
+end
+function ENT:NetGetForce()
+	return self:GetNetworkedInt("vecforce")/100
+end
+
+local Limit = .1
+local LastTime = 0
+function ENT:NetSetMul( mul )
+	--self:SetNetworkedBeamInt("vecmul", math.floor(mul*100))
+	if (CurTime() > LastTime + Limit) then
+		self:SetNetworkedInt("vecmul", math.floor((mul or 0)*100))
+		LastTime = CurTime()
+	end
+end
+function ENT:NetGetMul()
+	--return self:GetNetworkedBeamInt(5)/100
+	return self:GetNetworkedInt("vecmul")/100
+end
+
+function ENT:GetOverlayText()
+	local mode = self:GetMode()
+	return string.format("Force Mul: %.2f\nInput: %.2f\nForce Applied: %.2f\nMode: %s",
+		self:NetGetForce(),
+		self:NetGetMul(),
+		self:NetGetForce() * self:NetGetMul(),
+		(mode == 0 and "XYZ Local") or (mode == 1 and "XYZ World") or (mode == 2 and "XY Local, Z World")
+	)
+end
+
+
+if CLIENT then 
+	function ENT:Initialize()
+		self.ShouldDraw = 1
+
+		local mx, mn = self:GetRenderBounds()
+		self:SetRenderBounds(mn + Vector(0,0,128), mx, 0)
+	end
+
+	function ENT:DrawTranslucent()
+		if self.ShouldDraw == 0 or not self:IsOn() then return end
+
+		local EffectDraw = WireLib.ThrusterEffectDraw[self:GetEffect()]
+		if EffectDraw then EffectDraw(self) end
+	end
+
+	function ENT:Think()
+		self.BaseClass.Think(self)
+
+		self.ShouldDraw = GetConVarNumber("cl_drawthrusterseffects")
+
+		if self.ShouldDraw == 0 or not self:IsOn() then return end
+
+		local EffectThink = WireLib.ThrusterEffectThink[self:GetEffect()]
+		if EffectThink then EffectThink(self) end
+	end
+
+	function ENT:CalcNormal()
+		local mode = self:GetMode()
+		if mode == 1 then
+			return self:GetNormal()
+		elseif mode == 2 then
+			local v = self:GetNormal()
+			local z = v.z
+			v = self:LocalToWorld(Vector(v.x,v.y,0))
+			v.z = v.z + z
+			return (v - self:GetPos()):GetNormalized()
+		else
+			return (self:LocalToWorld(self:GetNormal()) - self:GetPos()):GetNormalized()
+		end
+	end
+	
+	return  -- No more client
+end
+
+-- Server
 
 function ENT:Initialize()
 	self:PhysicsInit( SOLID_VPHYSICS )
